f9fe4a1960cd08103e111d0258baff8246d6a122
Mango Shaman <eqmccullough@gmail.com>
2023-06-30 10:41:46 -0400

    complete functionality and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Employee-Tracker
+
+In this activity, I created an employee tracking application that connects to a MySQL database to return information and update information about the organization and its employees. This activity utilizes inquire as well as MySQL2 packages.
+
+# Github Reposity link
+
+https://github.com/eqmccullough/Employee-Tracker
+
+# Walkthrough video link
+
+https://drive.google.com/file/d/1O2rD6NXVzhH63cHOOvzhU_ZxqzSelc_b/view
--- server.js
+++ server.js
@@ -25,44 +25,80 @@ const db = mysql.createConnection(
 inquirer.prompt([
     {
       type: 'list',
-      message: 'What shape would you like to include in your logo?',
+      message: 'What would you like to do?',
       name: 'selection',
       choices: ['view all departments', 'view all roles', 'view all employees', 'add a department','add a role', 'add an employee',  'update an employee role'],
     },
   ]) .then((data) => {
-    //sends data to json file
-    console.log(data);
-    console.log(data.selection);
+    // console.log(data);
+    // console.log(data.selection);
     if(data.selection == 'view all departments'){
       db.query('SELECT * FROM department', function (err, results) {
      console.log(results);
-   } else if(data.selection == 'view all roles'){
+
+   })} else if(data.selection == 'view all roles'){
     db.query('SELECT * FROM roles', function (err, results) {
       console.log(results);
-   }
+   })
   } else if(data.selection == 'view all employees'){
     db.query('SELECT * FROM employee', function (err, results) {
       console.log(results);
-   }
+   })
   } else if(data.selection == 'add a department'){
-    db.query('SELECT * FROM employee', function (err, results) {
+
+    inquirer.prompt([
+      {
+      type: 'input',
+      name: 'departmentName',
+      message: "What is the department's name?",
+    }]).then((data2) => {
+
+      console.log(data2.departmentName);
+      const sql = "INSERT INTO department (name) VALUES (?)";
+      const params = data2.departmentName;
+      db.query(sql, params, function (err, results) {
+        if (err) throw err;
+      });
+      db.query('SELECT * FROM department', function (err, results) {
+        console.log(results);
+     })
+    });
+    }  else if(data.selection == 'add a role'){
+    inquirer.prompt([
+      {
+      type: 'input',
+      name: 'roleName',
+      message: "What is the role's name?",
+    },
+    {
+      type: 'input',
+      name: 'roleSalary',
+      message: "What is the role's salary?",
+    },
+    {
+      type: 'input',
+      name: 'roleDepartment',
+      message: "What is the role's department ID?",
+    },
+  ]).then((data3) => { 
+     const sql = "INSERT INTO roles (title, salary, department_id) VALUES (?)";
+     const params = [data3.roleName, data3.roleSalary, data3.roleDepartment];
+    db.query(sql, [params], (err, result) => {});
+    db.query('SELECT * FROM roles', function (err, results) {
       console.log(results);
-   } 
-  }else if(data.selection == 'add a role'){
-    db.query('SELECT * FROM employee', function (err, results) {
-      console.log(results);
-   }
+   })
+   });
   } else if(data.selection == 'add an employee'){
     inquirer.prompt([
       {
       type: 'input',
       name: 'employeefName',
-      message: 'What is the employee's firstname?',
+      message: "What is the employee's first name?",
     },
     {
       type: 'input',
       name: 'employeelName',
-      message: 'What is the employee's lastname?',
+      message: "What is the employee's last name?",
     },
     {
       type: 'input',
@@ -73,76 +109,51 @@ inquirer.prompt([
       type: 'input',
       name: 'managerID',
       message: "What is the employee's manageer's ID?",
-    },
-
-  }).then((data2) => {
-    const sql = 'INSERT INTO employee (first_name) VALUES (?)`;
-    const params = data2.employeefName;
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
+    },])
+    .then((data4) => {
+    const sql = "INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUES (?)";
+    const params = [data4.employeefName, data4.employeelName, data4.roleID, data4.managerID];
+    db.query(sql, [params], (err, result) => {});
+    db.query('SELECT * FROM employee', function (err, results) {
+      console.log(results);
+   })
     });
+  } else if(data.selection == 'update an employee role'){
+
+    db.query('SELECT id, first_name, last_name FROM employee', function (err, results) {
+      // console.log(results);
+    
+  
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'employeeSelect',
+        message: "Whose role would you like to update? type in their employee ID \n\n" + [results[0].id + " " + results[0].first_name + " " + results[0].last_name + "\n" + 
+        results[1].id + " " + results[1].first_name + " " + results[1].last_name + "\n" + 
+        results[2].id + " " + results[2].first_name + " " + results[2].last_name + "\n" + 
+        results[3].id + " " + results[3].first_name + " " + results[3].last_name + "\n" + 
+        results[4].id + " " + results[4].first_name + " " + results[4].last_name + "\n"]
+        
+      },
+      {
+        type: 'input',
+        name: 'newRole',
+        message: "What is his/her new role ID",
+    
+      },
+    ]) .then((data5) => {
+      // console.log(data5);
+      db.query( "SELECT * FROM employee WHERE id = (?)", data5.employeeSelect, (err, results) => {
+        console.log(results);
+      });
+      const sql = "UPDATE employee SET roles_id = (?) WHERE id = (?)";
+    const params = [data5.newRole, data5.employeeSelect];
+    db.query(sql, params, (err, result) => {});
+    db.query( "SELECT * FROM employee WHERE id = (?)", data5.employeeSelect, (err, results) => {
+      console.log(results);
     });
-  //   db.query(`INSERT INTO employee (first_name) VALUES (?)`, function (err, results) {
-  //     console.log(results);
-  //  }
-    
 
-
-  }
-    // db.query('SELECT * FROM employee', function (err, results) {
-    //   console.log(results);
-   }
-  // } else if(data.selection == 'update an employee role'){
-  //   db.query(`UPDATE employee SET roles_id = ? WHERE id = ?`, function (err, results) {
-  //     console.log(results);
-  //  }
-  // }
-      //   fs.writeFile("./lib/data.json", JSON.stringify(data, null, '\t'), (err) =>
-      //   err ? console.log(err) : console.log('Success!')
-      // );
-      // });
-
-
-    //   const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
-    //   const params = [req.body.review, req.params.id];
-    
-    //   db.query(sql, params, (err, result) => {
-    //     if (err) {
-    //       res.status(400).json({ error: err.message });
-    //     } else if (!result.affectedRows) {
-    //       res.json({
-    //         message: 'Movie not found'
-    //       });
-    //     } else {
-    //       res.json({
-    //         message: 'success',
-    //         data: req.body,
-    //         changes: result.affectedRows
-    //       });
-    //     }
-    //   });
-    // });
-      
-
-  // db.query('SELECT * FROM department', function (err, results) {
-  //   console.log(results);
-  // })
- 
-
-// db.query(`DELETE FROM favorite_books WHERE id = ?`, deletedRow, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
-
-// db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-//   console.log(results);
-// });
-
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
+  })
+});
+};
+});
